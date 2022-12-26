c457b1539f5664cccc07aeb6929ef5845077aacf
nykoh2001 <nykoh2001@gmail.com>
2022-12-26 09:31:28 +0900

    merge: git conflict

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-import Login from "./Login";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
     <div>
       <Login />
-    </div>
-=======
-import Header from "./components/organisms/Header";
-
-function App() {
-  return (
-    <>
-      <Header/>
-      <div>App</div>
-    </>
->>>>>>> 76b1c57099c77c4082fdaf9aa94e1a63d85e1f4d
-  );
+    </div>)
 }
 
 export default App;
