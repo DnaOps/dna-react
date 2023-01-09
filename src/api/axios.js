556d177ff3b3ea743f3c4c82c3a38248d1a62097
nykoh2001 <nykoh2001@gmail.com>
2023-01-09 12:28:21 +0900

    chore: env variable naming + ignore env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const Axios = axios.create({ baseURL: process.env.REACT_APP_API });
+const Axios = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 
 export default Axios;
