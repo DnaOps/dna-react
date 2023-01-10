f72ad388c49407070ad0861bb57a627e9bcb92d2
nykoh2001 <nykoh2001@gmail.com>
2023-01-10 16:58:00 +0900

    feat: modularization login request

--- src/api/request.js
+++ src/api/request.js
@@ -1,21 +1,9 @@
 import Axios from "./axios";
 
-export const getReq = (url, params) => {
-  Axios.get(url, params)
-    .then((res) => {
-      console.log("res: ", res);
-    })
-    .catch((e) => {
-      console.log("err: ", e);
-    });
-};
-
-export const postReq = (url, params) => {
-  Axios.post(url, params)
-    .then((res) => {
-      console.log("res: ", res);
-    })
-    .catch((e) => {
-      console.log("err: ", e);
-    });
+export const postAuthenticate = async (signInData) => {
+  const res = await Axios.post("/authenticate", signInData);
+  const jwt = res.data.data.jwt;
+  console.log("jwt: ", jwt);
+  localStorage.setItem("Authorization", jwt.accessToken);
+  // set refresh token in cookie
 };
