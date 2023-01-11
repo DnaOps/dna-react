c6457ecfca5450b6e5abd6bcdd1b7593335283f7
nykoh2001 <nykoh2001@gmail.com>
2023-01-11 13:59:47 +0900

    chore: import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Login from "./components/pages/Login";
 import Notify from "./components/pages/Notify";
+import NofityList from "./components/pages/NotifyList";
+import Onboard from "./components/pages/Onboard";
 
 function App() {
   return (
     <div>
-      <Notify />
+      <Login />
     </div>
-
   );
 }
 
