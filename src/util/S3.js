1fbda0c8250769c0702369d8a58d40ff72114312
nykoh2001 <nykoh2001@gmail.com>
2023-02-27 17:21:38 +0900

    feat: aws s3 object format

--- src/util/S3.js
+++ src/util/S3.js
@@ -6,18 +6,8 @@ const awsConfig = {
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_DEFAULT_REGION,
-}
+};
 
 const s3 = new AWS.S3(awsConfig);
 
-const uploadParams = {
-  Bucket: process.env.AWS_BUCKET,
-  Body: file,
-  Key: `image/${v1().toString().replace("-", "")}.${
-    file.type.split("/")[1]
-  }`,
-  ContentType: file.type,
-  ACL: "public-read",
-};
-
-const uploadImage = ()
+const uploadImage = () => {};
