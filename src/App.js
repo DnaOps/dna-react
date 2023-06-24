c930c0e0e6c9a5a700408fc91e34da25eab5c4df
nykoh2001 <nykoh2001@gmail.com>
2023-06-24 17:28:11 +0900

    feat: forum list

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ import SignUp from "./components/pages/SignUp";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Onboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/notify_list" element={<NotifyList type="notify" />} />
-      <Route path="/board_list" element={<NotifyList type="board" />} />
-      <Route path="/study_list" element={<NotifyList type="study" />} />
-      <Route path="/notify/:id" element={<Notify type="notify" />} />
-      <Route path="/board/:id" element={<Notify type="board" />} />
-      <Route path="/study/:id" element={<Notify type="study" />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Onboard />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<SignUp />} />
+			<Route path="/notify_list" element={<NotifyList type="notify" />} />
+			<Route path="/board_list" element={<NotifyList type="forum" />} />
+			<Route path="/study_list" element={<NotifyList type="study" />} />
+			<Route path="/notify/:id" element={<Notify type="notify" />} />
+			<Route path="/board/:id" element={<Notify type="forum" />} />
+			<Route path="/study/:id" element={<Notify type="study" />} />
+			<Route path="/home" element={<Home />} />
+		</Routes>
+	);
 }
 
 export default App;
--- src/api/request.js
+++ src/api/request.js
@@ -3,80 +3,96 @@ import { Axios, AxiosBeforeAuthored } from "./axios";
 import { setCookie } from "../util/Cookie";
 
 export const postAuthenticate = async (
-  signInData,
-  handleUserInfo,
-  redirectHome
+	signInData,
+	handleUserInfo,
+	redirectHome
 ) => {
-  try {
-    const res = await AxiosBeforeAuthored.post("auth/authenticate", signInData);
-    const userInfo = res.data.data.userInfoResponse;
-    const jwt = res.data.data.tokenResponse.jwt;
-    redirectHome();
-    localStorage.setItem("Authorization", `Bearer ${jwt.accessToken}`);
-    setCookie("refresh_token", jwt.refreshToken, { secure: true });
-    handleUserInfo(userInfo);
-  } catch (e) {
-    alert("로그인 정보가 일치하지 않습니다.");
-  }
+	try {
+		const res = await AxiosBeforeAuthored.post("auth/authenticate", signInData);
+		const userInfo = res.data.data.userInfoResponse;
+		const jwt = res.data.data.tokenResponse.jwt;
+		redirectHome();
+		localStorage.setItem("Authorization", `Bearer ${jwt.accessToken}`);
+		setCookie("refresh_token", jwt.refreshToken, { secure: true });
+		handleUserInfo(userInfo);
+	} catch (e) {
+		alert("로그인 정보가 일치하지 않습니다.");
+	}
 };
 
 export const getNaverLogin = async () => {
-  const res = await Axios.get("/oauth2/authorization/naver");
-  console.log("res: ", res);
+	const res = await Axios.get("/oauth2/authorization/naver");
+	console.log("res: ", res);
 };
 
 export const getNotices = async (boardInfo, inViewed) => {
-  const { start, offset, criteria, keyword } = boardInfo;
-  let res = null;
-  if (start === "" || offset === "") {
-    res = await Axios.get(`/boards/notices`);
-  } else if (criteria === "" || keyword === "") {
-    res = await Axios.get(`/boards/notices?start=${start}&offset=${offset}`);
-  } else {
-    res = await Axios.get(
-      `/boards/notices?start=${start}&offset=${offset}&criteria=${criteria}&keyword=${keyword}`
-    );
-  }
-  const noticeList = res.data.data.list;
-  inViewed(noticeList);
+	const { type, start, title, author, content } = boardInfo;
+
+	let res = null;
+	if (start == "") {
+		if (title == "" && author == "" && content == "")
+			res = await Axios.get(`/${type}Posts`);
+		else if (title && content)
+			res = await Axios.get(`/${type}Posts?title=${title}?content=${content}`);
+		else if (title) res = await Axios.get(`/${type}Posts?title=${title}`);
+		else if (author) res = await Axios.get(`/${type}Posts?author=${author}`);
+		else if (content) res = await Axios.get(`/${type}Posts?content=${content}`);
+	} else {
+		if (title == "" && author == "" && content == "")
+			res = await Axios.get(`/${type}Posts?start=${start}`);
+		else if (title && content)
+			res = await Axios.get(
+				`/${type}Posts?start=${start}?title=${title}?content=${content}`
+			);
+		else if (title)
+			res = await Axios.get(`/${type}Posts?start=${start}?title=${title}`);
+		else if (author)
+			res = await Axios.get(`/${type}Posts?start=${start}?author=${author}`);
+		else if (content)
+			res = await Axios.get(`/${type}Posts?start=${start}?content=${content}`);
+	}
+
+	const noticeList = res.data.data.list;
+
+	inViewed(noticeList);
 };
 
 export const getSpecificNotify = async (
-  id,
-  handleNotifyInfo,
-  handleComment
+	id,
+	handleNotifyInfo,
+	handleComment
 ) => {
-  const notifyRes = await Axios.get(`/boards/notices/${id}`);
-  handleNotifyInfo(notifyRes.data.data);
-  getNofityComments(id, handleComment);
+	const notifyRes = await Axios.get(`/boards/notices/${id}`);
+	handleNotifyInfo(notifyRes.data.data);
+	getNofityComments(id, handleComment);
 };
 
 export const getNofityComments = async (id, handleComment) => {
-  const res = await Axios.get(`/comments/notices/${id}`);
-  handleComment(res.data.data.list);
+	const res = await Axios.get(`/comments/notices/${id}`);
+	handleComment(res.data.data.list);
 };
 
 export const deleteNotify = async (id) => {
-  const res = await Axios.delete("/boards/notices", { data: { noticeId: id } });
-  console.log("res: ", res);
+	const res = await Axios.delete("/boards/notices", { data: { noticeId: id } });
+	console.log("res: ", res);
 };
 
 export const postNotifyComment = async (commentData, callBack) => {
-  await Axios.post("/comments", commentData);
-  callBack();
+	await Axios.post("/comments", commentData);
+	callBack();
 };
 
 export const postSignUp = async (signUpData) => {
-  const res = await Axios.post("/auth/signUp", signUpData);
-  console.log("res: ", res);
+	const res = await Axios.post("/auth/signUp", signUpData);
+	console.log("res: ", res);
 };
 
 export const getIfLiked = async (noticeId, handleLike) => {
-  const res = await Axios.get(`/likes/notices/${noticeId}`);
-  handleLike(res.data.data);
+	const res = await Axios.get(`/likes/notices/${noticeId}`);
+	handleLike(res.data.data);
 };
 
 export const postLike = async (noticeId, handleLike) => {
-  const res = await Axios.post(`/likes/notices/${noticeId}`);
-  handleLike(res.data.data);
+	const res = await Axios.post(`/likes/notices/${noticeId}`);
+	handleLike(res.data.data);
 };
